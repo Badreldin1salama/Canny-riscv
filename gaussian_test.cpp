abc4d605c9fa8bc2a1a8b46701568d0570697c6e
BassamKhodary <bassamkhodary1@gmail.com>
2026-06-20 00:06:22 +0300

    test(canny): upgrade and modify Gaussian and Sobel tests v3

--- gaussian_test.cpp
+++ gaussian_test.cpp
@@ -41,3 +41,74 @@ applyGaussianBlur(input.data(), output.data(), width, height);
     EXPECT_GT(output[center + 1], 0); // الجار الأيمن
     EXPECT_GT(output[center - 1], 0); // الجار الأيسر
 }
+
+// 4. اختبار تماثل الـ Gaussian Kernel (Symmetry Test)
+TEST_F(GaussianTest, SymmetryTest) {
+    int center = (height / 2) * width + (width / 2);
+    input[center] = 255;
+
+    applyGaussianBlur(input.data(), output.data(), width, height);
+
+    // يمين = يسار
+    EXPECT_EQ(output[center + 1], output[center - 1]);
+
+    // أعلى = أسفل
+    EXPECT_EQ(output[center + width], output[center - width]);
+
+    // القطرين أيضاً متماثلين
+    EXPECT_EQ(output[center + width + 1],
+              output[center - width - 1]);
+
+    EXPECT_EQ(output[center + width - 1],
+              output[center - width + 1]);
+}
+
+TEST_F(GaussianTest, CenterIsBrightestPixel)
+{
+    int center =
+        (height/2)*width +
+        (width/2);
+
+    input[center] = 255;
+
+    applyGaussianBlur(
+        input.data(),
+        output.data(),
+        width,
+        height);
+
+    EXPECT_GT(output[center],
+              output[center+1]);
+
+    EXPECT_GT(output[center],
+              output[center-1]);
+
+    EXPECT_GT(output[center],
+              output[center+width]);
+
+    EXPECT_GT(output[center],
+              output[center-width]);
+}
+
+// 6. اختبار الـ Zero Padding عند الحدود (Boundary Test)
+TEST_F(GaussianTest, BoundaryZeroPadding) {
+    std::fill(input.begin(), input.end(), 255);
+
+    applyGaussianBlur(input.data(), output.data(), width, height);
+
+    // الركن العلوي الأيسر
+    EXPECT_LT(output[0], 255);
+
+    // الركن العلوي الأيمن
+    EXPECT_LT(output[width - 1], 255);
+
+    // الركن السفلي الأيسر
+    EXPECT_LT(output[(height - 1) * width], 255);
+
+    // الركن السفلي الأيمن
+    EXPECT_LT(output[width * height - 1], 255);
+
+    // بكسل داخلي يجب أن يبقى قريباً من 255
+    int center = (height / 2) * width + (width / 2);
+    EXPECT_NEAR(output[center], 255, 1);
+}
